0efa2ab73d015281b7540a1b8717ef72796ada11
Djalma Bezerra <djalmacf@hotmail.com>
2025-03-18 00:57:29 -0300

    alteração de #

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  ngAfterViewInit() {
+    const navLinks = this.el.nativeElement.querySelectorAll('.nav-link');
+    const navbarToggler = this.el.nativeElement.querySelector('.navbar-toggler');
+    const navbarCollapse = this.el.nativeElement.querySelector('.navbar-collapse');
+
+    navLinks.forEach((link: HTMLElement) => {
+      this.renderer.listen(link, 'click', () => {
+        if (window.innerWidth < 992) { // Fecha apenas no mobile
+          navbarToggler.click(); // Simula o clique no botão para fechar o menu
+        }
+      });
+    });
+  }
+
+  reloadPage() {
+    window.location.reload();
+  }
 }
